4bd4285a1e0415fd8878eda72a1e91d2c12fc709
Alexey Morozov <alexeymorozov.dev@protonmail.com>
2021-02-06 10:20:43 +0300

    Video 5

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -27,5 +27,30 @@
                 @endforeach
             </div>
         </div>
+
+        @auth
+            <div class="row justify-content-center">
+                <div class="col-md-8">
+                    <form action="{{ $thread->path() . '/replies' }}" method="POST">
+                        @csrf
+                        <div class="form-group mt-4">
+                            <textarea
+                                name="body"
+                                id="body"
+                                class="form-control"
+                                rows="5"
+                                placeholder="Have something to say?"></textarea>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Post</button>
+                    </form>
+                </div>
+            </div>
+        @endauth
+
+        @guest
+            <p class="text-center mt-4">Please <a href="{{ route('login') }}">sign in</a> to participate in this discussion.</p>
+        @endguest
+
+
     </div>
 @endsection
