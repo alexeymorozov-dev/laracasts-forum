c7a23665276b126fb08e7b0e4fed7ee3d6d0b88c
Alexey Morozov <alexeymorozov.dev@protonmail.com>
2021-02-10 14:25:14 +0300

    Video 24

---
+++ app/Models/RecordsActivity.php
@@ -0,0 +1,45 @@
+<?php
+
+
+namespace App\Models;
+
+
+trait RecordsActivity
+{
+
+    protected static function bootRecordsActivity()
+    {
+        foreach (static::getActivitiesToRecord() as $event) {
+            static::$event(function ($model) use ($event) {
+                $model->recordActivity($event);
+            });
+        }
+    }
+
+    protected static function getActivitiesToRecord()
+    {
+        return ['created', 'deleted'];
+    }
+
+    public function recordActivity($event)
+    {
+        if (auth()->guest()) return;
+
+        $this->activity()->create([
+            'user_id' => auth()->id(),
+            'type' => $this->getActivityType($event),
+        ]);
+    }
+
+    public function activity()
+    {
+        return $this->morphMany('App\Models\Activity', 'subject');
+    }
+
+    protected function getActivityType($event)
+    {
+        $type = strtolower((new \ReflectionClass($this))->getShortName());
+
+        return "{$event}_{$type}";
+    }
+}
